6fefd4c5f3350c86c1cb76efc2d4c41c2abf414b
DESKTOP-B184KCC\Zyl <silencer191@yahoo.de>
2021-06-03 13:06:26 +0200

    Make packaging less of a hassle

--- package.bat
+++ package.bat
@@ -1,4 +1,6 @@
-mkdir mouse_strafing
-copy *.py mouse_strafing
-justzip -i mouse_strafing -o mouse_strafing.zip
-rmdir /S /Q mouse_strafing
+@mkdir mouse_strafing
+@copy *.py mouse_strafing
+@where justzip
+@if %ERRORLEVEL% NEQ 0 go install github.com/Zyl9393/justzip@v1
+@justzip -i mouse_strafing -o mouse_strafing.zip
+@rmdir /S /Q mouse_strafing
