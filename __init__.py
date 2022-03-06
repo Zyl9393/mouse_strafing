3193768f3f00ab3fba483f5600a0d666ef5bf5a4
DESKTOP-B184KCC\Zyl <silencer191@yahoo.de>
2022-03-06 17:56:55 +0100

    Bump version

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Mouse Strafing",
     "description": "Strafe in the 3D View using the mouse.",
     "author": "Zyl",
-    "version": (2, 3),
+    "version": (2, 4),
     "blender": (2, 92, 0),
     "category": "3D View"
 }
